d3961de9cdba5a54832f50ec4e3780c55d2d4ecc
Karol Musur <kamusur@gmail.com>
2017-10-17 20:31:22 +0200

    Add a search box

--- lib/activeadmin/views/google_map_proxy.rb
+++ lib/activeadmin/views/google_map_proxy.rb
@@ -6,11 +6,11 @@ module ActiveAdmin
       end
 
       def loading_map_code
-        @loading_map ? "<script src=\"https://maps.googleapis.com/maps/api/js?language=#{@lang}#{key}&callback=googleMapObject.init\" async defer></script>" : ''
+        @loading_map ? "<script src=\"https://maps.googleapis.com/maps/api/js?language=#{@lang}#{key}&libraries=places&callback=googleMapObject.init\" async defer></script>" : ''
       end
 
       def to_s
-        File.open(File.expand_path('../templates/google.html', __FILE__)).read % [loading_map_code, @height, @id_lat, @id_lng, @default_lat, @default_lng, @map_zoom]
+        File.open(File.expand_path('../templates/google.html', __FILE__)).read % [loading_map_code, @height, @id_lat, @id_lng, @map_zoom, @default_lat, @default_lng]
       end
     end
   end
